99565a24bb69ddf8b5b9d46f4dc671b02cc5ac31
weisj <weisj@arcor.de>
2020-03-15 12:18:20 +0100

    Fixed double installation on laf change.

--- macos/src/main/java/com/github/weisj/darklaf/platform/macos/ui/MacOSDecorationsUtil.java
+++ macos/src/main/java/com/github/weisj/darklaf/platform/macos/ui/MacOSDecorationsUtil.java
@@ -61,11 +61,6 @@ public class MacOSDecorationsUtil {
             boolean isDarkTheme = UIManager.getBoolean("Theme.dark");
             JNIDecorationsMacOS.setDarkTheme(windowHandle, isDarkTheme);
         }
-        if (jniInstall) {
-            Dimension size = window.getSize();
-            size.height += titleBarHeight;
-            window.setSize(size);
-        }
         return new DecorationInformation(windowHandle, fullWindowContent, transparentTitleBar,
                                          jniInstall, rootPane, titleVisible, titleBarHeight, titleFontSize);
     }
