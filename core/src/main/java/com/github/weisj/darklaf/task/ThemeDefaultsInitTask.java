af9c9afaab4781ea5db28dfae9b85f80284c6476
weisj <weisj@arcor.de>
2020-10-24 19:32:17 +0200

    Inline common icon color definition to reduce amount of properties.

--- core/src/main/java/com/github/weisj/darklaf/task/ThemeDefaultsInitTask.java
+++ core/src/main/java/com/github/weisj/darklaf/task/ThemeDefaultsInitTask.java
@@ -44,8 +44,8 @@ public class ThemeDefaultsInitTask implements DefaultsInitTask {
             "numberingPane", "optionPane", "panel", "popupMenu", "progressBar", "radioButton", "rootPane", "scrollBar",
             "scrollPane", "separator", "slider", "spinner", "splitPane", "statusBar", "tabbedPane", "tabFrame", "table",
             "taskPane", "text", "toggleButton", "toolBar", "toolTip", "tree", "misc"};
-    private static final String[] ICON_PROPERTIES = new String[] {"checkBox", "radioButton", "slider", "dialog",
-            "files", "frame", "indicator", "menu", "misc", "navigation", "progress"};
+    private static final String[] ICON_PROPERTIES =
+            new String[] {"checkBox", "radioButton", "slider", "files", "frame"};
     private static final String ACCENT_COLOR_KEY = "effectiveAccentColor";
     private static final String ACCENT_COLOR_BACKUP_KEY = "themeAccentColor";
     private static final String ACCENT_COLOR_SOURCE_KEY = "widgetFillDefault";
