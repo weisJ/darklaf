40e18bc667d17b7fb9b70ab84226cb417ef14cfd
weisj <31143295+weisJ@users.noreply.github.com>
2021-05-18 19:17:29 +0200

    Remove unnecessary methods from UIAction.

--- core/src/main/java/com/github/weisj/darklaf/ui/UIAction.java
+++ core/src/main/java/com/github/weisj/darklaf/ui/UIAction.java
@@ -43,33 +43,24 @@ public abstract class UIAction implements Action {
         return null;
     }
 
-    // UIAction is not mutable, this does nothing.
-    public void putValue(String key, Object value) {}
-
-    // UIAction is not mutable, this does nothing.
-    public void setEnabled(boolean b) {}
-
-    /**
-     * Cover method for <code>isEnabled(null)</code>.
-     */
-    public final boolean isEnabled() {
-        return accept(null);
-    }
-
-    /**
-     * Subclasses that need to conditionalize the enabled state should override this. Be aware that
-     * <code>sender</code> may be null.
-     *
-     * @param sender Widget enabled state is being asked for, may be null.
-     */
     @Override
-    public boolean accept(Object sender) {
+    public final boolean isEnabled() {
         return true;
     }
 
     // UIAction is not mutable, this does nothing.
+    @Override
+    public void putValue(String key, Object value) {}
+
+    // UIAction is not mutable, this does nothing.
+    @Override
+    public void setEnabled(boolean b) {}
+
+    // UIAction is not mutable, this does nothing.
+    @Override
     public void addPropertyChangeListener(PropertyChangeListener listener) {}
 
     // UIAction is not mutable, this does nothing.
+    @Override
     public void removePropertyChangeListener(PropertyChangeListener listener) {}
 }
