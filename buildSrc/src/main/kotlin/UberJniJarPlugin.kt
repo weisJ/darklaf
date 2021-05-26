f0674937d22d4b605775acc1c968a08c7df34f16
weisj <31143295+weisJ@users.noreply.github.com>
2021-05-26 16:57:48 +0200

    Build: Workaround to allow publishing the library. see: https://github.com/nokeedev/gradle-native/issues/272 and https://github.com/nokeedev/gradle-native/issues/276

--- buildSrc/src/main/kotlin/UberJniJarPlugin.kt
+++ buildSrc/src/main/kotlin/UberJniJarPlugin.kt
@@ -6,6 +6,7 @@ import org.gradle.api.Project
 import org.gradle.api.file.FileTree
 import org.gradle.api.provider.Provider
 import org.gradle.jvm.tasks.Jar
+import org.gradle.nativeplatform.tasks.LinkSharedLibrary
 
 class UberJniJarPlugin : Plugin<Project> {
 
@@ -20,16 +21,39 @@ class UberJniJarPlugin : Plugin<Project> {
         val logger = task.logger
         val library = project.extensions.getByType(JniLibraryExtension::class.java)
 
+        // Prevent variants from being published.
+        val targetMachines = library.targetMachines.forUseAtConfigurationTime().get()
+        val variantNames = targetMachines.map { "${project.name}-${it.architectureString}" }
+        project.configurations.forEach { config ->
+            config.artifacts.removeIf { it.name in variantNames }
+        }
+
         logger.info("${project.name}: Merging binaries into the JVM Jar.")
-        task.from(library.variants.flatMap { variant ->
-            if (variant.targetMachine.targetsHost) {
-                variant.binaries.withType(JniJarBinary::class.java)
-                    .map { it.asZipTree(project) }.get()
-            } else listOf()
-        })
+        library.variants.configureEach {
+            binaries.withType(JniJarBinary::class.java).configureEach {
+                jarTask.configure { enabled = false }
+            }
+            task.dependsOn(sharedLibrary.linkTask)
+        }
+        targetMachines.forEach { target ->
+            val targetVariant = library.variants.filter { it.targetMachine == target }.map {
+                check(it.size == 1)
+                it.first()
+            }
+            if (!target.targetsHost) {
+                // nativeRuntimeFiles will be populated in this case due to using pre-build binaries.
+                task.from(targetVariant.map { it.nativeRuntimeFiles }) {
+                    into(targetVariant.map { it.resourcePath })
+                }
+            } else {
+                task.from(targetVariant.map { it.sharedLibrary.linkTask.map { linkTask -> linkTask.linkedFile } }) {
+                    into(targetVariant.map { it.resourcePath })
+                }
+            }
+        }
     }
 
-    private fun JarBinary.asZipTree(project: Project): Provider<FileTree> =
+    private fun JniJarBinary.asZipTree(project: Project): Provider<FileTree> =
         jarTask.map { project.zipTree(it.archiveFile) }
 
 }
