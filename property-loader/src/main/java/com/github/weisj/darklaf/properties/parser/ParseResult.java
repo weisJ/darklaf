3c7ec99cd6cf976824f5b9fdcfcdccd12eeb8486
Jannis Weis <31143295+weisJ@users.noreply.github.com>
2022-02-02 20:23:24 +0100

    Parser: Trim input before parsing

--- property-loader/src/main/java/com/github/weisj/darklaf/properties/parser/ParseResult.java
+++ property-loader/src/main/java/com/github/weisj/darklaf/properties/parser/ParseResult.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2019-2021 Jannis Weis
+ * Copyright (c) 2019-2022 Jannis Weis
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
  * associated documentation files (the "Software"), to deal in the Software without restriction,
@@ -35,7 +35,7 @@ public class ParseResult {
 
     public ParseResult(final String key, final String value) {
         this.key = key != null ? key : "";
-        this.value = value != null ? value : "";
+        this.value = value != null ? value.trim() : "";
     }
 
     @Override
