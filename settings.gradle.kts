cb7e40f215f63f0f77f67239bba0577affc5217d
Jannis Weis <31143295+weisJ@users.noreply.github.com>
2022-07-31 20:26:26 +0200

    Fix indentation

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,7 +16,7 @@ pluginManagement {
 }
 
 plugins {
-  id("dev.nokee.nokee-version-management") version("1.0.0")
+    id("dev.nokee.nokee-version-management") version("1.0.0")
 }
 
 dependencyResolutionManagement {
