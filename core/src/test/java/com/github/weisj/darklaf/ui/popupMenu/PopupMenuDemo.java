72e13ba55ad2c3dd4e6784274dc3a7da00a37e17
Jannis Weis <31143295+weisJ@users.noreply.github.com>
2023-04-27 23:31:58 +0200

    Demo: Add Menu with only menu items to PopupMenuDemo

--- core/src/test/java/com/github/weisj/darklaf/ui/popupMenu/PopupMenuDemo.java
+++ core/src/test/java/com/github/weisj/darklaf/ui/popupMenu/PopupMenuDemo.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2019-2021 Jannis Weis
+ * Copyright (c) 2019-2023 Jannis Weis
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
  * associated documentation files (the "Software"), to deal in the Software without restriction,
@@ -155,6 +155,13 @@ public class PopupMenuDemo extends BaseComponentDemo {
                     {
                         for (int i = 0; i < 10; i++) {
                             add(new JCheckBoxMenuItem("Item " + i));
+                        }
+                    }
+                },
+                new JMenu("Some Items") {
+                    {
+                        for (int i = 0; i < 5; i++) {
+                            add(new JMenuItem("Item " + i));
                         }
                     }
                 },
