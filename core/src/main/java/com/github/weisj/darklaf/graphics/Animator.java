ed6958d8dacfeb6b3ea8f6c454f0f381b042b33a
Jannis Weis <31143295+weisJ@users.noreply.github.com>
2025-02-19 17:57:40 +0100

    Ensure onAnimationFinished is called on EDT
    Fixes #358

--- core/src/main/java/com/github/weisj/darklaf/graphics/Animator.java
+++ core/src/main/java/com/github/weisj/darklaf/graphics/Animator.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2019-2024 Jannis Weis
+ * Copyright (c) 2019-2025 Jannis Weis
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
  * associated documentation files (the "Software"), to deal in the Software without restriction,
@@ -291,7 +291,11 @@ public abstract class Animator {
      */
     public void stop() {
         if (cancel()) {
-            onAnimationFinished();
+            if (SwingUtilities.isEventDispatchThread()) {
+                onAnimationFinished();
+            } else {
+                SwingUtilities.invokeLater(this::onAnimationFinished);
+            }
         }
     }
 
